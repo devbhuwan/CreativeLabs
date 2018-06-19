ee1e23d409a1df2406b17eb741301c4597ec784c
developerbhuwan <bhuwan.upadhyay49@gmail.com>
2018-06-19 21:49:09 +0400

    BR: documentation

--- binary-librarian/src/main/java/com/developerbhuwan/binarylibrarian/BinaryLibrarianApplication.java
+++ binary-librarian/src/main/java/com/developerbhuwan/binarylibrarian/BinaryLibrarianApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BinaryLibrarian {
+public class BinaryLibrarianApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BinaryLibrarian.class, args);
+        SpringApplication.run(BinaryLibrarianApplication.class, args);
     }
 }
